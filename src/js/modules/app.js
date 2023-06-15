f65c3bbaed77e663283738062e693ea3b3ee3baa
jannis2301 <jannis.ax@gmx.de>
2023-06-15 20:56:36 +0200

    feat: added noscroll & keyboard-access

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -3,6 +3,7 @@ import { Year } from './year.js'
 
 export class App {
   constructor() {
+    this.document = document.documentElement
     this.overlay = document.querySelector('.overlay')
     this.sidebar = document.querySelector('.sidebar')
     this.openMenuBtn = document.querySelector('.openMenuBtn')
@@ -12,9 +13,26 @@ export class App {
     this.yearComponent = new Year(this)
   }
 
+  handleMenuViaDocument = (e) => {
+    if (e.type === 'click') {
+      e.stopPropagation()
+      if (!this.sidebar.classList.contains('openMenu')) {
+        return
+      } else if (e.target.nodeName === 'ASIDE') {
+        this.menu.closeMenu()
+      }
+    } else if (e.key === 'Escape') {
+      this.menu.closeMenu()
+    } else if (e.key === 'Enter') {
+      this.menu.openMenu()
+    }
+  }
+
   init = () => {
     this.openMenuBtn.addEventListener('click', this.menu.openMenu)
     this.closeMenuBtn.addEventListener('click', this.menu.closeMenu)
+    this.document.addEventListener('click', this.handleMenuViaDocument)
+    this.document.addEventListener('keydown', this.handleMenuViaDocument)
     this.yearComponent.calculateCurrentYear()
   }
 }
--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -3,17 +3,32 @@ export class Menu {
     this.app = app
   }
 
-  openMenu = () => {
+  getDurationTime = () => {
+    return (
+      parseFloat(
+        getComputedStyle(this.app.document)
+          .getPropertyValue('--animation-duration')
+          .slice(0, -1)
+      ) * 1000
+    )
+  }
+
+  openMenu = (e) => {
+    if (e) e.stopPropagation()
     this.app.overlay.classList.add('showOverlay')
     setTimeout(() => {
       this.app.sidebar.classList.add('openMenu')
     }, 50)
+    this.app.document.classList.add('noScroll')
   }
 
-  closeMenu = () => {
+  closeMenu = (e) => {
+    if (e) e.stopPropagation()
+    this.app.document.classList.remove('noScroll')
+    const durationTime = this.getDurationTime()
     this.app.sidebar.classList.remove('openMenu')
     setTimeout(() => {
       this.app.overlay.classList.remove('showOverlay')
-    }, 300)
+    }, durationTime)
   }
 }
