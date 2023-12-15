90487b9ce123421f58bc7c6eafd7c6375f03270b
jannis2301 <jannis2301@users.noreply.github.com>
2023-12-15 19:48:25 +0000

    deploy: bf713d664a05bc18d5a91aace2a66e109a210d25

--- src/Sidebar.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Sidebar', () => {
-  it('should be a sidebar element', () => {
-    const sidebarElement = 'SidebarElement';
-    expect(sidebarElement).toBe('SidebarElement');
-  });
-});
