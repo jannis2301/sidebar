bf713d664a05bc18d5a91aace2a66e109a210d25
jannis2301 <jannis.ax@gmx.de>
2023-12-15 20:37:30 +0100

    test: add a test environment

---
+++ src/Sidebar.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Sidebar', () => {
+  it('should be a sidebar element', () => {
+    const sidebarElement = 'SidebarElement';
+    expect(sidebarElement).toBe('SidebarElement');
+  });
+});
